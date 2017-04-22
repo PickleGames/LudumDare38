e6fd7b10793dbf58dba53144245891a1f11db877
Miguel Garnica <mgarnica123@aol.com>
2017-04-21 19:11:41 -0800

    Seal PlaceHolder

---
+++ Ludum Dare 38/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+    float move;
+    public float jumpForce;
+
+    // ground check variables
+    bool grounded;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
