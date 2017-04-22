5e090c5b3626dd4a61f47f542bfb81685379ca8c
Miguel Garnica <mgarnica123@aol.com>
2017-04-21 21:07:06 -0800

    - Player Movement Script

--- Ludum Dare 38/Assets/Scripts/PlayerMovement.cs
+++ Ludum Dare 38/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour {
 
     Rigidbody2D rb;
@@ -12,16 +13,47 @@ public class PlayerMovement : MonoBehaviour {
     // ground check variables
     bool grounded;
     public Transform groundCheck;
-    float groundRadius = 0.2f;
+    float groundRadius = .25f;
     public LayerMask whatIsGround;
+
+    private Transform myTransform;
+
+    private GameObject seal;
+   
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
+        myTransform = GetComponent<Transform>();
+        seal = this.gameObject.transform.GetChild(0).gameObject;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void FixedUpdate()
+    {
+        // check if doge is on ground
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        // get WASD/ARROW input and move doge accordingly
+        move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (grounded && Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+
+        if (rb.velocity.x > 0)
+        {
+           seal.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            seal.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+    }
 }
