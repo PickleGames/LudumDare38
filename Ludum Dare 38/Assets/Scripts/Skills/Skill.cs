8aa195e52c2e09b86fa3e6de9bbd4d8fa3604bdd
Nguyen Duong <ducnguyenduchao@gmail.com>
2017-04-21 23:41:53 -0700

    Added Skill class template

---
+++ Ludum Dare 38/Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Skill : MonoBehaviour {
+
+    public float defaultCoolDown;
+    public GameObject player;
+    private float timeCoolDown;
+    private bool IsUse;
+    private bool IsUsable;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        if (IsUse)
+        {
+            if(timeCoolDown >= defaultCoolDown)
+            {
+                IsUsable = true;
+                IsUse = false;
+            }
+            timeCoolDown += Time.deltaTime;
+        }
+	}
+
+    public void Use() {
+        IsUse = true;
+    }
+
+    public abstract void Function();
+
+}
---
+++ Ludum Dare 38/Assets/Scripts/Skills/Slap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slap : Skill { 
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+
+    public override void Function()
+    {
+        
+    }
+
+}
