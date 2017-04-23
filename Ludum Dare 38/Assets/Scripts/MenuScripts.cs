ee87bf3053fcabb1d13ac773d726c5ad34fb8350
Harris Yun <harrispooted2@gmail.com>
2017-04-23 15:32:14 -0700

    menu

---
+++ Ludum Dare 38/Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour {
+
+	public string mainSceneName = null;
+	//goes to main game scene
+	public void playButtonFunction() {
+		SceneManager.LoadScene(mainSceneName);
+	}
+
+	//quits game
+	public void quitButtonFunction() {
+		Application.Quit();
+	}
+
+
+}
