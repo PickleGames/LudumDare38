c399cf9a7fe09453fd6a1d7ba4b480b016f6a1bb
Miguel Garnica <mgarnica123@aol.com>
2017-04-23 15:22:43 -0700

    Score UI/ Increment (slap seals to get score)

---
+++ Ludum Dare 38/Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        scoreText.text = "Score: " + score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        scoreText.text = "Score: " + score.ToString();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject go = collision.gameObject;
+        Debug.Log("WE HAVE COLLIDED!!!");
+        if (go.CompareTag("enemy"))
+        {
+            Debug.Log("INCREMENTING SCORE!!");
+            score += 1;
+        }
+    }
+}
