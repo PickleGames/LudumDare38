92a077fe827a0a7248d3403fed6a4199dd500c66
Nguyen Duong <ducnguyenduchao@gmail.com>
2017-04-24 15:04:23 -0700

    Added: Ice shrinking to flatform

---
+++ Ludum Dare 38/Assets/Scripts/MeltingIce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeltingIce : MonoBehaviour {
+
+    //NEED AJUST
+    public GameObject player;
+    private float time;
+    private float timeElapsed;
+
+	void Start () {
+        time = player.transform.GetChild(6).GetComponent<Score>().timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeElapsed += Time.deltaTime;
+	    if(timeElapsed >= 5f)
+        {
+            Vector3 scale = this.transform.localScale;
+            scale.x *= .90f;
+            this.transform.localScale = scale;
+            timeElapsed = 0;
+        }	
+	}
+}
